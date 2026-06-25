f85cb2a9680d17020f00ec9fccd53a299067928d
wp2code <liu_weipinglove@163.com>
2026-06-25 12:29:34 +0800

    feat: 优化测试用例

--- src/test/java/com/lsy/idea/util/JsonUtilTest.java
+++ src/test/java/com/lsy/idea/util/JsonUtilTest.java
@@ -1,7 +1,8 @@
 package com.lsy.idea.util;
 
-import org.apache.http.util.Asserts;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author liuweiping
@@ -13,7 +14,7 @@ public class JsonUtilTest {
     public void testJsonToObject() {
         String json = "{\"name\":\"张三\",\"age\":20}";
         Person person = JsonUtils.fromJson(json, Person.class);
-        Asserts.notNull(person, "person should not be null");
+        assertNotNull(person, "person should not be null");
     }
     
     public static class Person {
