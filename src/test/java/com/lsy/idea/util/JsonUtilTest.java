28786febe80ddbd96c2beea2c12dd4e413bf3fe0
wp2code <liu_weipinglove@163.com>
2026-06-25 12:44:03 +0800

    feat: 优化测试用例

--- src/test/java/com/lsy/idea/util/JsonUtilTest.java
+++ src/test/java/com/lsy/idea/util/JsonUtilTest.java
@@ -22,7 +22,7 @@ public class JsonUtilTest {
         
         private String name;
         
-        private int age;
+        private Integer age;
         
         public String getName() {
             return name;
@@ -32,11 +32,11 @@ public class JsonUtilTest {
             this.name = name;
         }
         
-        public int getAge() {
+        public Integer getAge() {
             return age;
         }
         
-        public void setAge(int age) {
+        public void setAge(Integer age) {
             this.age = age;
         }
     }
